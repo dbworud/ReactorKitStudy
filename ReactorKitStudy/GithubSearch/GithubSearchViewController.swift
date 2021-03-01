b86ce1da87acf596528eb1a1abc3cc801023c40d
JaekyungYou <youjk96@gmail.com>
2021-03-01 11:18:53 +0900

    feat: complete bind viewcontroller with reactor

--- ReactorKitStudy/GithubSearch/GithubSearchViewController.swift
+++ ReactorKitStudy/GithubSearch/GithubSearchViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import ReactorKit
 import RxSwift
 import RxCocoa
-
+import SafariServices
 
 class GithubSearchViewController: UIViewController {
     
@@ -65,7 +65,43 @@ class GithubSearchViewController: UIViewController {
     }
     
     func bind(reactor: GithubSearchReactor) {
+        // Action in UI
+        searchController.searchBar.rx.text
+            .throttle(.milliseconds(300), scheduler: MainScheduler.instance)
+            .map { GithubSearchReactor.Action.updateQuery($0)}
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
+        tableView.rx.contentOffset
+            .filter { [weak self] offset in
+                guard let `self` = self else { return false }
+                guard self.tableView.frame.height > 0 else { return false }
+                
+                return offset.y + self.tableView.frame.height >= self.tableView.contentSize.height - 100
+            }
+            .map { _ in GithubSearchReactor.Action.loadNextPage}
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        // State
+        reactor.state.map { $0.repos }
+            .bind(to: tableView.rx.items(cellIdentifier: GithubSearchCell.identifier)) { indexPath, repo, cell in
+                cell.textLabel?.text = repo
+            }
+            .disposed(by: disposeBag)
+        
+        // View
+        tableView.rx.itemSelected
+            .subscribe(onNext: { [weak self, weak reactor] indexPath in
+                guard let `self` = self else { return }
+                self.view.endEditing(true)
+                self.tableView.deselectRow(at: indexPath, animated: false)
+                guard let repo = reactor?.currentState.repos[indexPath.row] else { return }
+                guard let url = URL(string: "https://github.com/\(repo)") else { return }
+                let viewController = SFSafariViewController(url: url)
+                self.searchController.present(viewController, animated: true, completion: nil)
+            })
+            .disposed(by: disposeBag)
     }
 }
 
